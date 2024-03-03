f789221e5cbc0c4c87b2e0240517cdfed4d5608c
reckyy <srreki98@gmail.com>
2024-03-03 13:15:50 +0900

    ⚡️ feat: todo一覧画面作成

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-blue-100">
+      <body className="bg-green-100">
         <Main />
         <NextScript />
       </body>
